06c28f55a54d1d3090a08044369a56b77e74762c
Janna <jannasheyanova@gmail.com>
2023-06-16 18:08:37 +0300

    Resolved task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        
+        int account = 67; //баланс пользователя
+        int refill = 1600; //сумма пополнения
+        int bonus;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        }   else {
+            bonus = 0;
+        }
+        int amount = account + refill + bonus;
+        System.out.println("Ваш баланс = " + amount + ". Количество зачисленных бонусов = " + bonus);
     }
 }
