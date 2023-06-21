58f9a3a894a724feb60017c0675bcc34c50b72bc
Janna <jannasheyanova@gmail.com>
2023-06-21 13:03:07 +0300

    Reformated code

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
         int account = 67; //баланс пользователя
         int refill = 1600; //сумма пополнения
         int bonus;
         if (refill > 1000) {
             bonus = refill / 100;
-        }   else {
+        } else {
             bonus = 0;
         }
         int amount = account + refill + bonus;
